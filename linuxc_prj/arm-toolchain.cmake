80a06dad6921d67075a6a365a89a1f613cce2f2d
wangshengyang <shengyangw_mail@163.com>
2025-09-12 21:08:25 +0800

    support soc S5P6818

--- linuxc_prj/arm-toolchain.cmake
+++ linuxc_prj/arm-toolchain.cmake
@@ -2,12 +2,16 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# 指定交叉编译器路径
-set(CMAKE_C_COMPILER /home/wsy/SDK/BSP_RK3506/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER /home/wsy/SDK/BSP_RK3506/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-g++)
+# 指定交叉编译器路径 / 设置库搜索路径（根据实际路径调整）
+#RK3506
+#set(CMAKE_C_COMPILER /home/wsy/sdk/BSP_RK3506/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-gcc)
+#set(CMAKE_CXX_COMPILER /home/wsy/sdk/BSP_RK3506/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-g++)
+#set(CMAKE_FIND_ROOT_PATH /home/wsy/sdk/BSP_RK3506/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc)
 
-# 设置库搜索路径（根据实际路径调整）
-set(CMAKE_FIND_ROOT_PATH /home/wsy/SDK/BSP_RK3506/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc)
+#S5P6818
+#set(CMAKE_C_COMPILER /opt/FriendlyARM/toolchain/6.4-aarch64/bin/aarch64-linux-gcc)
+#set(CMAKE_CXX_COMPILER /opt/FriendlyARM/toolchain/6.4-aarch64/bin/aarch64-linux-g++)
+#set(CMAKE_FIND_ROOT_PATH /opt/FriendlyARM/toolchain/6.4-aarch64/aarch64-cortexa53-linux-gnu/sysroot)
 
 # 搜索策略：只在目标环境查找库
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
