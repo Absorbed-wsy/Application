cdf0789fc33d552d85d8bbd7ba67ba02d4786911
wangshengyang <shengyangw_mail@163.com>
2025-09-12 12:52:50 +0800

    support thread_pool

--- linuxc_prj/src/app/main.c
+++ linuxc_prj/src/app/main.c
@@ -2,11 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "../core/logger.h"
 #include "../core/config.h"
+#include "../core/process_pool.h"
+#include "../core/thread_pool.h"
 
+void PrivateTask(void *arg);
+
+
+void print_helper(char **cmdline)
+{
+    printf("\nUsage:\n");
+    printf("%s gpio <gpiochip path> <gpio number> <direction: in/out> [<value: 0/1>]\n", cmdline[0]);
+}
 
 int config_initialize(const char* filename)
 {
@@ -18,15 +30,31 @@ int config_initialize(const char* filename)
     logger_init(NULL, debug, 1);
     LOG_DEBUG("Main debug level = %d",debug);
 
-
+    //other config
 
     config_free(conf);
 
     return 0;
 }
 
+int command_parsing(char **cmdline)
+{
+    const char *cmd = cmdline[1];
 
-int main(int argc, char *argv[]) {
+    if(!strcmp(cmd, "loop")) {
+        return 1;
+    } else if(!strcmp(cmd, "gpio")) {
+
+    } else {
+        print_helper(cmdline);
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) 
+{
+    int loop_status=0;
     
     //log init
     logger_init(NULL, LOG_LEVEL_INFO, 1);
@@ -35,14 +63,37 @@ int main(int argc, char *argv[]) {
     //config init
     config_initialize("app.conf");
 
+    //command parsing
+    if(argc > 1)
+        loop_status = command_parsing(argv);
 
+    //process thread task
+    LOG_INFO("Making threadpool with 4 threads");
+    threadpool thpool = thpool_init(4);
 
+    LOG_INFO("Adding 4 tasks to threadpool");
+	for (int i=0; i<4; i++){
+		thpool_add_work(thpool, PrivateTask, (void*)(uintptr_t)i);
+	};
 
+    while (loop_status) {
+        LOG_INFO("main loop");
+        sleep(1);
+    }
 
+    LOG_INFO("Killing threadpool");
+    thpool_wait(thpool);
+	thpool_destroy(thpool);
 
-    //while (1) {
-    //    sleep(1);
-    //}
+    LOG_INFO("Program over");
     
-    return 0;
+    exit(0);
+}
+
+void PrivateTask(void* arg) {
+    int value = (int)(uintptr_t)arg;
+    for(;;) {
+        printf("Thread #%lu working on %d\n", (unsigned long)pthread_self(), value);
+        sleep(1);
+    }
 }
---
+++ linuxc_prj/src/app/usage.md
@@ -0,0 +1,48 @@
+# =================================================关于线程池的使用方法======================================================
+int main(int argc, char *argv[]) 
+{
+    int loop_status=0;
+    
+    //log init
+    logger_init(NULL, LOG_LEVEL_INFO, 1);
+    LOG_INFO("Program started");
+
+    //config init
+    config_initialize("app.conf");
+
+    //command parsing
+    if(argc > 1)
+        loop_status = command_parsing(argv);
+
+    //process thread task
+    LOG_INFO("Making threadpool with 4 threads");
+    threadpool thpool = thpool_init(2);
+
+    LOG_INFO("Adding 4 tasks to threadpool");
+	for (int i=0; i<4; i++){
+		thpool_add_work(thpool, PrivateTask, (void*)(uintptr_t)i);
+	};
+
+    while (loop_status) {
+        LOG_INFO("main loop");
+        sleep(1);
+    }
+
+    LOG_INFO("Killing threadpool");
+    thpool_wait(thpool);
+	thpool_destroy(thpool);
+
+    LOG_INFO("Program over");
+    
+    exit(0);
+}
+
+void PrivateTask(void* arg) {
+    int value = (int)(uintptr_t)arg;
+    for(;;) {
+        printf("Thread #%lu working on %d\n", (unsigned long)pthread_self(), value);
+        sleep(1);
+    }
+}
+# ==========================================================================================================================
+
