49c9fef1389568840c0a8706a295460a96a323ab
wangshengyang <shengyangw_mail@163.com>
2025-09-26 10:37:58 +0800

    add string util

---
+++ linuxc_prj/src/util/string_util.c
@@ -0,0 +1,43 @@
+// src/util/string_util.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "string_util.h"
+
+//int puts(const char *s);
+//int putchar(int c);
+//int fputc(int c, FILE *stream);
+//int fputs(const char *s, FILE *stream);
+
+//char *gets(char *s);
+//int getchar(void);
+//char *fgets(char *s, int size, FILE *stream);
+//int fgetc(FILE *stream);
+
+//size_t strlen(const char *s);
+//char *strcat(char *dest, const char *src);
+//char *strcpy(char *dest, const char *src);
+//char *strncpy(char *dest, const char *src, size_t n);
+//void *memset(void *s, int c, size_t n);
+//void bzero(void *s, size_t n);
+//int strcmp(const char *s1, const char *s2);
+//int strncmp(const char *s1, const char *s2, size_t n);
+//char *strchr(const char *s, int c);
+//char *strrchr(const char *s, int c);
+//char *strstr(const char *haystack, const char *needle);
+
+//int atoi(const char *nptr);
+//long atol(const char *nptr);
+//long long atoll(const char *nptr);
+//double atof(const char *nptr);
+
+//long int strtol(const char *nptr, char **endptr, int base);
+//long long int strtoll(const char *nptr, char **endptr, int base);
+
+//unsigned long int strtoul(const char *nptr, char **endptr, int base);
+//unsigned long long int strtoull(const char *nptr, char **endptr, int base);
+
+//double strtod(const char *nptr, char **endptr);
+//float strtof(const char *nptr, char **endptr);
+//long double strtold(const char *nptr, char **endptr);
---
+++ linuxc_prj/src/util/string_util.h
@@ -0,0 +1,7 @@
+// src/util/string_util.h
+#ifndef STRING_UTIL_H
+#define STRING_UTIL_H
+
+
+
+#endif // STRING_UTIL_H
