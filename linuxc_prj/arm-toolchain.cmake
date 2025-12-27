926e771edcad3564c139daa369ffafae4ae7bd94
wsy <shengyangw_mail@163.com>
2025-12-27 23:57:41 +0800

    support cross compile libgpiod

--- linuxc_prj/arm-toolchain.cmake
+++ linuxc_prj/arm-toolchain.cmake
@@ -6,10 +6,16 @@ get_filename_component(PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}" DIRECTORY)
 
 # 指定交叉编译器路径 / 设置库搜索路径（根据实际路径调整）
 #RK3399
-set(TOOLCHAIN_DIR "${PROJECT_ROOT}/../BSP_RK3399/prebuilts/gcc/linux-x86/aarch64/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu")
+#set(TOOLCHAIN_DIR "${PROJECT_ROOT}/../BSP_RK3399/prebuilts/gcc/linux-x86/aarch64/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu")
+#set(CMAKE_C_COMPILER "${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-gcc")
+#set(CMAKE_CXX_COMPILER "${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++")
+#set(CMAKE_FIND_ROOT_PATH "${TOOLCHAIN_DIR}/aarch64-none-linux-gnu/libc")
+
+set(TOOLCHAIN_DIR "${PROJECT_ROOT}/../Toolchain/aarch64-rk-linux_gcc-10.3.1")
 set(CMAKE_C_COMPILER "${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++")
 set(CMAKE_FIND_ROOT_PATH "${TOOLCHAIN_DIR}/aarch64-none-linux-gnu/libc")
+include_directories(${TOOLCHAIN_DIR}/aarch64-none-linux-gnu/libc/include)
 
 # 搜索策略：只在目标环境查找库
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
--- linuxc_prj/scripts/build-libgpiod.sh
+++ linuxc_prj/scripts/build-libgpiod.sh
@@ -6,19 +6,19 @@
 current_path=$(pwd)
 
 # 定义环境变量
-export TOOLCHAIN_DIR="$HOME/sdk/BSP_RK3506/prebuilts/gcc/linux-x86/arm/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin"
-export TARGET_PLATFORM="arm-none-linux-gnueabihf"
+export TOOLCHAIN_DIR="/home/wsy/SDK/Toolchain/aarch64-rk-linux_gcc-10.3.1/bin"
+export TARGET_PLATFORM="aarch64-none-linux-gnu"
 export CROSS_COMPILE="${TARGET_PLATFORM}-"
 export CC="${CROSS_COMPILE}gcc"
 export CXX="${CROSS_COMPILE}g++"
 export PATH="$TOOLCHAIN_DIR:$PATH"
 
-export INSTALL_PATH=$current_path/build/libgpiod-2.1/install
+export INSTALL_PATH=/home/wsy/SDK/Toolchain/aarch64-rk-linux_gcc-10.3.1/aarch64-none-linux-gnu/libc
 
 # 创建安装目录
-tar -xf linuxc_prj/third_party/libgpiod-2.1.tar.gz -C build
+tar -xf ./third_party/libgpiod-1.6.x.tar.gz -C build
 mkdir -p ${INSTALL_PATH}
-cd $current_path/build/libgpiod-2.1
+cd ./build/libgpiod-1.6.x
 
 ./autogen.sh  # 生成 configure 脚本
 ./configure \
